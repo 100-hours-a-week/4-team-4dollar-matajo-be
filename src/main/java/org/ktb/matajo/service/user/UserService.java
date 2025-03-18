6b90c86a651fb2755ba2b5f45a559e812ca55825
iack23 <pogi12021@naver.com>
2025-03-18 18:08:16 +0900

    feat: UserService에 닉네임 중복 검사 기능 추가
    - isNicknameAvailable 메서드 구현
    - UserRepository를 활용하여 닉네임 중복 여부 확인
    - 닉네임이 없으면 true 반환

--- src/main/java/org/ktb/matajo/service/user/UserService.java
+++ src/main/java/org/ktb/matajo/service/user/UserService.java
@@ -1,3 +1,16 @@
 package org.ktb.matajo.service.user;
 
-public class UserService {}
+import lombok.RequiredArgsConstructor;
+import org.ktb.matajo.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public boolean isNicknameAvailable(String nickname) {
+        return !userRepository.existsByNickname(nickname); // 닉네임이 없으면 true 반환
+    }
+}
