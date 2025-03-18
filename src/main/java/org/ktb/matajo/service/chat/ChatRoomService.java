e7461377780965c11a96d626cc348ab56462396c
ghkddlscks19 <ghkddlscks19@naver.com>
2025-03-18 16:42:15 +0900

    feat: 채팅방 서비스 인터페이스 구현

--- src/main/java/org/ktb/matajo/service/chat/ChatRoomService.java
+++ src/main/java/org/ktb/matajo/service/chat/ChatRoomService.java
@@ -2,8 +2,14 @@ package org.ktb.matajo.service.chat;
 
 import org.ktb.matajo.dto.chat.ChatRoomCreateRequestDto;
 import org.ktb.matajo.dto.chat.ChatRoomCreateResponseDto;
+import org.ktb.matajo.dto.chat.ChatRoomResponseDto;
 
-public interface ChatService {
+import java.util.List;
+
+public interface ChatRoomService {
     // 채팅방 생성
     ChatRoomCreateResponseDto createChatRoom(ChatRoomCreateRequestDto chatRoomCreateRequestDto, Long userId);
+
+    // 채팅방 목록 조회
+    List<ChatRoomResponseDto> getMyChatRooms(Long userId);
 }
