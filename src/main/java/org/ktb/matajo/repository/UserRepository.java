9160ffbfdd2e4f3c0160602067ca0fc72b965234
iack23 <pogi12021@naver.com>
2025-03-18 17:43:24 +0900

    feat: UserRepository에 닉네임 중복 검사 메서드 추가
    - existsByNickname(String nickname) 메서드 구현
    - 사용자 닉네임 중복 여부 확인 가능

--- src/main/java/org/ktb/matajo/repository/UserRepository.java
+++ src/main/java/org/ktb/matajo/repository/UserRepository.java
@@ -1,3 +1,10 @@
 package org.ktb.matajo.repository;
 
-public class UserRepository {}
+import org.ktb.matajo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByNickname(String nickname);
+}
