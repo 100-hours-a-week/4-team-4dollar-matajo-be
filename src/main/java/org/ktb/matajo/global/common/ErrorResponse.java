549e3f7ef495c8f55471aa9e2faff649c56aa0c4
HYUN <8135zxcvbn@gmail.com>
2025-03-18 15:39:07 +0900

    feat: 공통 응답 및 예외 처리 기능 개선
    - CommonResponse 클래스에 데이터 필드를 추가하여 오류 응답 시 추가 정보를 포함할 수 있도록 수정
    - 새로운 ErrorResponse 클래스를 추가하여 유효성 검증 실패 시 필드별 오류 정보를 담는다.
    - ErrorCode 열거형에 새로운 오류 코드와 설명을 추가하여 비즈니스 규칙 위반 및 입력값 검증 오류를 보다 구체적으로 처리
    - GlobalExceptionHandler 클래스를 추가하여 다양한 예외를 처리하고, 적절한 오류 응답을 생성하는 기능 구현

---
+++ src/main/java/org/ktb/matajo/global/common/ErrorResponse.java
@@ -0,0 +1,17 @@
+package org.ktb.matajo.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String code;
+    private String message;
+    private Map<String, String> validation;  // 유효성 검증 실패 시 필드별 오류
+}
--- src/main/java/org/ktb/matajo/global/error/exception/BusinessException.java
+++ src/main/java/org/ktb/matajo/global/error/exception/BusinessException.java
@@ -1,8 +1,11 @@
 package org.ktb.matajo.global.error.exception;
 
+import lombok.Getter;
 import org.ktb.matajo.global.error.code.ErrorCode;
 
 public class BusinessException extends RuntimeException {
+
+    @Getter
     private final ErrorCode errorCode;
 
     public BusinessException(ErrorCode errorCode){
