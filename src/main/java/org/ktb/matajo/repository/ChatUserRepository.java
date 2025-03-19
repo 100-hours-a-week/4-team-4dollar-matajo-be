e3e2c2b90b70407bf86ab64cd7e1e204d7f91648
ghkddlscks19 <ghkddlscks19@naver.com>
2025-03-19 16:22:43 +0900

    feat: ChatUserRepository 기능 추가
    - 사용자 기준 활성화된 채팅 리스트 조회
    - 해당 채팅방의 사용자 참여 정보 조회

--- src/main/java/org/ktb/matajo/repository/ChatUserRepository.java
+++ src/main/java/org/ktb/matajo/repository/ChatUserRepository.java
@@ -1,13 +1,19 @@
 package org.ktb.matajo.repository;
 
+import org.ktb.matajo.entity.ChatRoom;
 import org.ktb.matajo.entity.ChatUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChatUserRepository extends JpaRepository<ChatUser, Long> {
-    // 채팅 리스트 조회
+    // 사용자 기준 활성화된 채팅 리스트 조회
     List<ChatUser> findByUserIdAndActiveStatusIsTrue(Long userId);
+
+    // 해당 채팅방의 사용자 참여 정보 조회
+    Optional<ChatUser> findByChatRoomAndUserId(ChatRoom chatRoom, Long userId);
+
 }
