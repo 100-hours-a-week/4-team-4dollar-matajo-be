71355fa3691ae9a218ce0309b671cbefee0547c2
iack23 <pogi12021@naver.com>
2025-03-19 15:28:10 +0900

    fix: UserRequestDto 유효성 검사 메시지 수정
    - API 명세에 맞게 validation 메시지 변경
    - @NotBlank 및 @Size 어노테이션 메시지를 'invalid_nickname'으로 통일
    - 닉네임 입력 조건(2~20자) 검증 로직 유지

--- src/main/java/org/ktb/matajo/dto/user/UserRequestDto.java
+++ src/main/java/org/ktb/matajo/dto/user/UserRequestDto.java
@@ -1,3 +1,15 @@
 package org.ktb.matajo.dto.user;
 
-public class UserRequestDto {}
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Getter
+@Setter
+public class UserRequestDto {
+
+    @NotBlank(message = "invalid_nickname") // ✅ API 명세에 맞게 수정
+    @Size(min = 2, max = 20, message = "invalid_nickname") // ✅ API 명세에 맞게 수정
+    private String nickname;
+}
