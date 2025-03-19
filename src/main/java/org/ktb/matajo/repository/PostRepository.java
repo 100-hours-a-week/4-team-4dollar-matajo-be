185ad39da25be6dad91e32dbc45ea38646456dce
HYUN <8135zxcvbn@gmail.com>
2025-03-19 22:46:19 +0900

    feat: 게시글, 태그 리포지토리 추가
    - PostRepository: 게시글 관련 데이터 접근을 위한 JPA 리포지토리 구현
      - 활성 게시글 목록 조회 및 주소 정보 포함 조회 기능 추가
      - 특정 게시글 ID로 활성 게시글 조회 기능 추가
    - TagRepository: 태그 관련 데이터 접근을 위한 JPA 리포지토리 구현
      - 태그 이름으로 태그 조회 및 이름 포함 검색 기능 추가

---
+++ src/main/java/org/ktb/matajo/repository/PostRepository.java
@@ -0,0 +1,30 @@
+package org.ktb.matajo.repository;
+
+import io.lettuce.core.dynamic.annotation.Param;
+import org.ktb.matajo.entity.Post;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT p FROM Post p WHERE p.deletedAt IS NULL ORDER BY p.createdAt DESC")
+    List<Post> findAllActivePostsOrderByCreatedAtDesc(Pageable pageable);
+
+    // 주소 조회시 sido, sigungu 데이터만 가져옴
+    @Query("SELECT p, a.sido, a.sigungu FROM Post p JOIN p.address a WHERE p.deletedAt IS NULL ORDER BY p.createdAt DESC")
+    List<Object[]> findAllActivePostsWithAddressInfoOrderByCreatedAtDesc(Pageable pageable);
+
+    // 리포지토리에 특화된 메서드 추가
+    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.address WHERE p.id = :id")
+    Optional<Post> findByIdWithAddress(@Param("id") Long id);
+
+
+    @Query("SELECT p FROM Post p WHERE p.id = :id AND p.deletedAt IS NULL")
+    Optional<Post> findActivePostById(@Param("id") Long id);
+}
