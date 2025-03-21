01d388c96b9a93bdb1c35ff58b52101d3a3ada9e
ghkddlscks19 <ghkddlscks19@naver.com>
2025-03-21 09:06:18 +0900

    feat: 테스트용 게시글 레포지토리 추가

---
+++ src/main/java/org/ktb/matajo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package org.ktb.matajo.repository;
+
+import org.ktb.matajo.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
