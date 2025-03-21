8ee6688f21f990e29e604ad6e0f4f48f1973a305
FromKyoung <kkyoung0611@naver.com>
2025-03-21 11:55:11 +0900

    feat: 신규 사용자 저장 및 JWT/리프레시 토큰 발급 로직 추가

---
+++ src/main/java/org/ktb/matajo/repository/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package org.ktb.matajo.repository;
+
+import org.ktb.matajo.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKakaoId(Long kakaoId);
+}
--- src/main/java/org/ktb/matajo/repository/UserRepository.java
+++ src/main/java/org/ktb/matajo/repository/UserRepository.java
@@ -1,3 +1,12 @@
 package org.ktb.matajo.repository;
 
-public class UserRepository {}
+import org.ktb.matajo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByKakaoId(Long kakaoId);
+    boolean existsByNickname(String nickname);
+}
