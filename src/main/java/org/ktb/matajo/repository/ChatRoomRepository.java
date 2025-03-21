51eed8cee82fc968f69bc35f404706c20491cd41
ghkddlscks19 <ghkddlscks19@naver.com>
2025-03-21 11:30:39 +0900

    chore: 미사용 import 구문 제거

--- src/main/java/org/ktb/matajo/repository/ChatRoomRepository.java
+++ src/main/java/org/ktb/matajo/repository/ChatRoomRepository.java
@@ -1,9 +1,7 @@
 package org.ktb.matajo.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import org.ktb.matajo.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
