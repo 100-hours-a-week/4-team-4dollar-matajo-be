797efdf001c46df9d2e46f85bd22abcbb7506dd7
ghkddlscks19 <ghkddlscks19@naver.com>
2025-03-22 16:06:22 +0900

    feat: 거래 정보 서비스 인터페이스 정의
    - 거래 정보 생성 메서드 선언
    - 내 거래 내역 조회 메서드 선언
    - 서비스 계층 구조 설계를 위한 인터페이스 구현

--- src/main/java/org/ktb/matajo/service/trade/TradeInfoService.java
+++ src/main/java/org/ktb/matajo/service/trade/TradeInfoService.java
@@ -1,8 +1,15 @@
 package org.ktb.matajo.service.trade;
 
-import org.ktb.matajo.dto.chat.ChatMessageRequestDto;
-import org.ktb.matajo.dto.chat.ChatMessageResponseDto;
+import org.ktb.matajo.dto.trade.TradeInfoListResponseDto;
+import org.ktb.matajo.dto.trade.TradeInfoRequestDto;
+import org.ktb.matajo.dto.trade.TradeInfoResponseDto;
 
-public interface TradeService {
-    ChatMessageResponseDto saveMessage(Long roomId, ChatMessageRequestDto messageDto);
+import java.util.List;
+
+public interface TradeInfoService {
+    // 거래 정보 생성
+    TradeInfoResponseDto createTrade(TradeInfoRequestDto tradeInfoRequestDto, Long userId);
+
+    // 내 거래 내역 조회
+    List<TradeInfoListResponseDto> getMyTrades(Long userId);
 }
