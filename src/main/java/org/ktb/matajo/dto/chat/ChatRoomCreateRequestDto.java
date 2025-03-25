1b9ecca38da2bca80a7dff86280aabda5df7980b
ghkddlscks19 <ghkddlscks19@naver.com>
2025-03-25 19:39:47 +0900

    feat: 유효성 검사 추가

--- src/main/java/org/ktb/matajo/dto/chat/ChatRoomCreateRequestDto.java
+++ src/main/java/org/ktb/matajo/dto/chat/ChatRoomCreateRequestDto.java
@@ -1,6 +1,7 @@
 package org.ktb.matajo.dto.chat;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 
@@ -9,6 +10,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class ChatRoomCreateRequestDto {
-    @NotNull
+    @NotNull(message = "게시글 ID는 필수 항목입니다")
+    @Positive(message = "게시글 ID는 양수여야 합니다")
     private Long postId; // 게시글 ID
 }
