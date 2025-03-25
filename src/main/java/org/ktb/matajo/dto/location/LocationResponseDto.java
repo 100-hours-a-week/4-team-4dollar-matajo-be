7a914bbc742ab86aaf6f5651a085255d2ab057b6
HYUN <8135zxcvbn@gmail.com>
2025-03-25 09:18:00 +0900

    feat: 위치 기반 게시글 조회 기능 및 DTO 추가
    - LocationController 클래스를 추가하여 특정 위치의 게시글 목록을 조회하는 API를 구현
    - LocationCompleteDto 및 LocationResponseDto 클래스를 추가하여 위치 정보와 게시글 정보를 담는 DTO를 정의
    - 위치 정보 ID를 기반으로 게시글 목록을 조회하는 getPostsByLocation 메서드를 구현
    - 위치 검색 기능을 위한 getSearchByLocation 메서드의 기본 구조를 추가

--- src/main/java/org/ktb/matajo/dto/location/LocationResponseDto.java
+++ src/main/java/org/ktb/matajo/dto/location/LocationResponseDto.java
@@ -1,10 +1,11 @@
-package org.ktb.matajo.dto.post;
+package org.ktb.matajo.dto.location;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+import org.ktb.matajo.entity.Address;
 
 /** 위치 기반 게시글 정보 응답 DTO Post ID와 Address 정보 포함 */
 @Getter
@@ -14,5 +15,7 @@ import lombok.NoArgsConstructor;
 public class LocationResponseDto {
   private Long postId;
   private String address;
+  private float latitude;
+  private float longitude;
   // private Address address;
 }
