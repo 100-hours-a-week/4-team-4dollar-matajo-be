a217defaf3c70d616041a078eb3b3a77b6897cf5
HYUN <8135zxcvbn@gmail.com>
2025-03-27 20:43:18 +0900

    fix: 클라이언트 식별자 반환 로직 수정
    - RateLimitConfig 클래스의 getClientIdentifier 메서드에서 JWT 토큰에서 사용자 ID 대신 사용자 닉네임을 반환하도록 수정

--- src/main/java/org/ktb/matajo/config/RateLimitConfig.java
+++ src/main/java/org/ktb/matajo/config/RateLimitConfig.java
@@ -81,12 +81,10 @@ public class RateLimitConfig {
             // JWT 토큰에서 파싱된 정보인 경우
             if (authentication.getPrincipal() instanceof Map) {
                 Map<String, Object> principal = (Map<String, Object>) authentication.getPrincipal();
-                if (principal.containsKey("Id")) {
-                    return "user:" + principal.get("Id");
+                if (principal.containsKey("nickname")) {
+                    return "user:" + principal.get("nickname");
                 }
             }
-
-
         }
 
         // 인증되지 않은 경우, IP 주소 사용
