45a144433b1887b156b570f35f01364040fff660
ghkddlscks19 <ghkddlscks19@naver.com>
2025-03-27 14:17:09 +0900

    feat: 문자열 이스케이프 및 언이스케이프

---
+++ src/main/java/org/ktb/matajo/config/HTMLCharacterEscapes.java
@@ -0,0 +1,34 @@
+package org.ktb.matajo.config;
+
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
+import com.fasterxml.jackson.core.io.SerializedString;
+import org.apache.commons.text.StringEscapeUtils;
+
+public class HTMLCharacterEscapes extends CharacterEscapes {
+
+    private final int[] asciiEscapes;
+
+    public HTMLCharacterEscapes() {
+
+        // 기본 ASCII 이스케이프 설정 가져오기
+        asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
+
+        // XSS 공격에 사용될 수 있는 문자들 이스케이프 처리 설정
+        asciiEscapes['<'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['>'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['&'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['\"'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
+    }
+
+    @Override
+    public int[] getEscapeCodesForAscii() {
+        return asciiEscapes;
+    }
+
+    @Override
+    public SerializableString getEscapeSequence(int i) {
+        return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) i)));
+    }
+}
