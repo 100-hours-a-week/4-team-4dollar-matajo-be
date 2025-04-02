372601930805ede4a17bd18e0c4725b2c33b8a60
ghkddlscks19 <ghkddlscks19@naver.com>
2025-04-02 09:00:23 +0900

    feat: rateLimiter 주석으로만 추가

---
+++ src/main/java/org/ktb/matajo/config/WebConfig.java
@@ -0,0 +1,26 @@
+package org.ktb.matajo.config;
+
+import lombok.RequiredArgsConstructor;
+//import org.ktb.matajo.interceptor.RateLimitInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+
+public class WebConfig implements WebMvcConfigurer {
+
+//    private final RateLimitInterceptor rateLimitInterceptor;
+
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry){
+//        registry.addInterceptor(rateLimitInterceptor)
+//            .addPathPatterns("/**") //모든 경로 적용
+//            .excludePathPatterns(
+//                "/swagger-ui/**", //API 문서 제외
+//                "/v3/api-docs/**" // API 문서 제외
+//            );
+//    }
+}
