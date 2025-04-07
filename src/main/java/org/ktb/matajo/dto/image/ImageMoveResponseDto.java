bebef97533659dde9bd5a6a43abb8db844af817a
ghkddlscks19 <ghkddlscks19@naver.com>
2025-04-07 19:55:18 +0900

    feat: 프론트와 lambda 사이의 중개 역할 기능 추가

---
+++ src/main/java/org/ktb/matajo/dto/image/ImageMoveResponseDto.java
@@ -0,0 +1,23 @@
+package org.ktb.matajo.dto.image;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.util.List;
+
+/**
+ * 이미지 이동 응답 DTO
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Schema(description = "이미지 이동 응답 DTO")
+public class ImageMoveResponseDto {
+    
+    @Schema(description = "이동된 이미지 목록")
+    private List<MovedImageDto> movedImages;
+    
+    @Schema(description = "실패한 이미지 목록")
+    private List<String> failedImages;
+}
