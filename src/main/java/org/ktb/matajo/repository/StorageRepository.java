f2dcda535aacff3aad16e17184548e004b7f2740
iack23 <pogi12021@naver.com>
2025-04-24 13:52:06 +0900

    feat(repository): Storage 엔티티용 Repository 생성
    - JpaRepository<Storage, Long> 상속
    - locationInfoId 기준 조회 기능(findByLocationInfoId) 추가

---
+++ src/main/java/org/ktb/matajo/repository/StorageRepository.java
@@ -0,0 +1,9 @@
+package org.ktb.matajo.repository;
+
+import org.ktb.matajo.entity.Storage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface StorageRepository extends JpaRepository<Storage, Long> {
+    List<Storage> findByLocationInfoId(Long locationInfoId);
+}
