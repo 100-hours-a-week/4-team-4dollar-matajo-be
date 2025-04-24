d2f2667f3bbd23c1b9e4085959c35d8ec4a85ac5
iack23 <pogi12021@naver.com>
2025-04-24 17:29:54 +0900

    chore: StorageRepository 줄바꿈만 수정

--- src/main/java/org/ktb/matajo/repository/StorageRepository.java
+++ src/main/java/org/ktb/matajo/repository/StorageRepository.java
@@ -2,6 +2,7 @@ package org.ktb.matajo.repository;
 
 import org.ktb.matajo.entity.Storage;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface StorageRepository extends JpaRepository<Storage, Long> {
