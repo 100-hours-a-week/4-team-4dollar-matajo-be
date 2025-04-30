37589de0ea3ea92fe68a3282c44ad7ea33067394
ghkddlscks19 <ghkddlscks19@naver.com>
2025-04-30 09:29:26 +0900

    feat: 커밋으로 저장

---
+++ src/main/java/org/ktb/matajo/controller/S3Controller.java
@@ -0,0 +1,64 @@
+package org.ktb.matajo.controller;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/s3")
+public class S3Controller {
+    private AmazonS3 s3Client;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @PostConstruct
+    public void init() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        this.s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = createFileName(file.getOriginalFilename());
+
+        try {
+            s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw new RuntimeException("S3 업로드 실패: " + e.getMessage());
+        }
+
+        return s3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private String createFileName(String originalName) {
+        String extension = originalName.substring(originalName.lastIndexOf("."));
+        return UUID.randomUUID().toString() + extension;
+    }
+}
